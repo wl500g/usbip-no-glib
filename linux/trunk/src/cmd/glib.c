ac8aeb1e82117e216459b58f83dc3f40c7de4053
Vladislav Grishenko <themiron@mail.ru>
2011-01-15 21:43:34 +0500

    add builabe glib2 stub

---
+++ linux/trunk/src/cmd/glib.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <glib.h>
+
+struct _GMainContext
+{
+	gint ref_count;
+};
+
+struct _GMainLoop
+{
+	gint ref_count;
+	GMainContext *context;
+	gboolean is_running;
+};
+
+struct _GIOChannel
+{
+	gint ref_count;
+	GIOFuncs *funcs;
+	int fd;
+};
+
+struct _GIOFuncs
+{
+	GIOStatus (*io_read) (void);
+	GIOStatus (*io_write) (void);
+	GIOStatus (*io_seek) (void);
+	GIOStatus (*io_close) (void);
+//	GSource* (*io_create_watch) (void);
+	void (*io_free) (void);
+	GIOStatus (*io_set_flags) (void);
+	GIOFlags (*io_get_flags) (void);
+};
+
+GMainLoop _mainloop;
+GMainContext _maincontext;
+
+GMainLoop *
+g_main_loop_new(GMainContext *context, gboolean is_running)
+{
+	_mainloop.context = (context == NULL) ? &_maincontext : context;
+	_mainloop.is_running = is_running;
+	return &_mainloop;
+}
+
+void
+g_main_loop_run(GMainLoop *loop)
+{
+	loop->is_running = TRUE;
+	while (loop->is_running)
+	    sleep(1);
+}
+
+void
+g_main_loop_quit(GMainLoop *loop)
+{
+	loop->is_running = FALSE;
+}
+
+GIOChannel _channel;
+
+GIOChannel *
+g_io_channel_unix_new(int fd)
+{
+	_channel.fd = fd;
+	return &_channel;
+}
+
+gint
+g_io_channel_unix_get_fd(GIOChannel *channel)
+{
+	return channel->fd;
+}
+
+guint
+g_io_add_watch(GIOChannel *channel, GIOCondition condition, GIOFunc func, gpointer user_data)
+{
+	return 0;
+}
--- linux/trunk/src/cmd/glib.h
+++ linux/trunk/src/cmd/glib.h
@@ -53,17 +53,24 @@ typedef enum
 #define g_strerror(x)	strerror(x)
 #define gai_strerror(x)	strerror(x)
 
-typedef void GMainContext;
-typedef void GMainLoop;
+typedef struct _GMainContext GMainContext;
+typedef struct _GMainLoop GMainLoop;
 
-GMainLoop *g_main_loop_new        (GMainContext *context,
-			    	   gboolean      is_running);
-void       g_main_loop_run        (GMainLoop    *loop);
-void       g_main_loop_quit       (GMainLoop    *loop);
-GMainLoop *g_main_loop_ref        (GMainLoop    *loop);
-void       g_main_loop_unref      (GMainLoop    *loop);
-gboolean   g_main_loop_is_running (GMainLoop    *loop);
-GMainContext *g_main_loop_get_context (GMainLoop    *loop);
+GMainLoop *g_main_loop_new(GMainContext *context, gboolean is_running);
+void g_main_loop_run(GMainLoop *loop);
+void g_main_loop_quit(GMainLoop *loop);
+GMainLoop *g_main_loop_ref(GMainLoop *loop);
+void g_main_loop_unref(GMainLoop *loop);
+gboolean g_main_loop_is_running(GMainLoop *loop);
+GMainContext *g_main_loop_get_context(GMainLoop *loop);
+
+typedef enum
+{
+  G_IO_STATUS_ERROR,
+  G_IO_STATUS_NORMAL,
+  G_IO_STATUS_EOF,
+  G_IO_STATUS_AGAIN
+} GIOStatus;
 
 typedef enum
 {
@@ -75,6 +82,24 @@ typedef enum
   G_IO_NVAL,
 } GIOCondition;
 
-typedef void GIOChannel;
+typedef enum
+{
+  G_IO_FLAG_APPEND = 1 << 0,
+  G_IO_FLAG_NONBLOCK = 1 << 1,
+  G_IO_FLAG_IS_READABLE = 1 << 2,	/* Read only flag */
+  G_IO_FLAG_IS_WRITEABLE = 1 << 3,	/* Read only flag */
+  G_IO_FLAG_IS_SEEKABLE = 1 << 4,	/* Read only flag */
+  G_IO_FLAG_MASK = (1 << 5) - 1,
+  G_IO_FLAG_GET_MASK = G_IO_FLAG_MASK,
+  G_IO_FLAG_SET_MASK = G_IO_FLAG_APPEND | G_IO_FLAG_NONBLOCK
+} GIOFlags;
+
+typedef struct _GIOChannel GIOChannel;
+typedef struct _GIOFuncs GIOFuncs;
+typedef gboolean (*GIOFunc) (GIOChannel *source, GIOCondition condition, gpointer data);
+
+GIOChannel* g_io_channel_unix_new(int fd);
+gint g_io_channel_unix_get_fd(GIOChannel *channel);
+guint g_io_add_watch(GIOChannel *channel, GIOCondition condition, GIOFunc func, gpointer user_data);
 
 #endif
